a2eccf889fd57a90fc367c1864c599976b7898c0
Daniel Eni Kandil <denikandil@proofpoint.com>
2025-03-07 16:29:44 +0200

    logger

--- tradeflow-backend/utils/logger.py
+++ tradeflow-backend/utils/logger.py
@@ -1,29 +1,46 @@
 import logging
 import os
 
-# Define a new log level TRACE that is lower than DEBUG.
-TRACE_LEVEL_NUM = 5
-logging.addLevelName(TRACE_LEVEL_NUM, "TRACE")
 
-def trace(self, message, *args, **kws):
-    if self.isEnabledFor(TRACE_LEVEL_NUM):
-        # Yes, logger takes its '*args' as 'args'.
-        self._log(TRACE_LEVEL_NUM, message, args, **kws)
+class Logger:
+    TRACE_LEVEL_NUM = 5  # Define a new TRACE log level
 
-logging.Logger.trace = trace
+    def __init__(self):
+        self._setup_trace_level()
+        self.log_level = self._determine_log_level()
+        self._configure_logging()
+        self._configure_flask_migrate_logging()
 
-# Determine if we're running locally or in AWS.
-# A common practice is to check an environment variable. For AWS, 'AWS_EXECUTION_ENV' is typically present.
-if os.environ.get("AWS_EXECUTION_ENV"):
-    log_level = logging.INFO
-else:
-    # Running locally (or not in AWS), use TRACE
-    log_level = TRACE_LEVEL_NUM
+    def _setup_trace_level(self):
+        """Adds a custom TRACE log level to logging."""
+        logging.addLevelName(self.TRACE_LEVEL_NUM, "TRACE")
 
-# Configure the root logger.
-logging.basicConfig(
-    level=log_level,
-    format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
-)
+        def trace(self, message, *args, **kws):
+            if self.isEnabledFor(Logger.TRACE_LEVEL_NUM):
+                self._log(Logger.TRACE_LEVEL_NUM, message, args, **kws)
 
+        logging.Logger.trace = trace
+
+    def _determine_log_level(self):
+        """Determines the log level based on the execution environment."""
+        if os.environ.get("AWS_EXECUTION_ENV"):
+            return logging.INFO  # Running in AWS
+        return self.TRACE_LEVEL_NUM  # Running locally
+
+    def _configure_logging(self):
+        """Configures the root logger."""
+        logging.basicConfig(
+            level=self.log_level,
+            format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        )
+
+    def _configure_flask_migrate_logging(self):
+        """Ensures Flask-Migrate logging follows the same log level."""
+        logging.getLogger("flask_migrate").setLevel(self.log_level)
+
+
+# Initialize Logger
+Logger()
+
+# Use log as the main logger instance outside the class
 log = logging.getLogger(__name__)
